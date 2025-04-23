63a5d8ea46c6e609dbdb228ec9c392a0a7ecfe4a
Luca Giusiano <luca.giusiano@vallauri.edu>
2025-04-23 10:56:32 +0200

    Commit di prova
    Commit prova per classe

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 "use strict";
 
-const RIGHE=6;
-const COLONNE=7;
+const RIGHE = 6;
+const COLONNE = 7;
 
 const GREY = "rgb(127, 127, 127)";
 const YELLOW = "rgb(255, 255, 0)";
 const RED = "rgb(255, 0, 0)";
 
 
-window.onload=function(){
-
- 
-
+window.onload = function () {
+    alert("ciao");
 }
